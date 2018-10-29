d01acd4eb42cb78efa9ac135abf11b5469168c7e
Jeremy Cook <jeremy.cook@cloudacademy.com>
2018-10-29 14:56:00 +1300

    updates

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React - Jeremy
+            Learn React - Jeremy Cook
           </a>
         </header>
       </div>
