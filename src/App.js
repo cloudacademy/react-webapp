1d91309110fe75504e2c440434aae42fb6abd41c
eddyattang <eddy.attang@kinandcarta.com>
2022-07-06 21:23:09 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           >
           </a>
           <h3>
-            CloudAcademy ❤ DevOps 2019 - version 2.0
+            CloudAcademy ❤ DevOps 2019 - version 2.2
           </h3>
         </header>
       </div>
