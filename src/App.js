5d3c716339f8bfc3a7c66bd58ad9310ec0b4808d
yemisie <86678242+yemisie@users.noreply.github.com>
2023-03-07 22:39:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           >
           </a>
           <h3>
-            CloudAcademy ❤ DevOps 2019 - version 2.0
+            CloudAcademy ❤ DevOps 2023 - version 2.0
           </h3>
         </header>
       </div>
